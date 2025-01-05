86743fb2273de20901263b50f6a363e14ad112a0
grzegorz.l <grzegorzlasek01@gmail.com>
2025-01-05 22:38:34 +0100

    fix push

---
+++ Backend/src/Controllers/RentalController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoRentalService.Models;
+using VideoRentalService.Services;
+
+namespace VideoRentalService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalController : ControllerBase
+    {
+        private readonly RentalService _rentalService;
+
+        public RentalController(RentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
+        // Get all rentals for user
+        [HttpGet]
+        public async Task<ActionResult<List<Rental>>> GetAllRentalsForUser(int userId)
+        {
+            return await _rentalService.GetRentalsForUserAsync(userId);
+        }
+
+        //Add movie to rental
+        [HttpPost]
+        public async Task<ActionResult> AddMovieToRental(int userId, int movieId)
+        {
+            await _rentalService.AddRentalMovieAsync(userId, movieId);
+            return Ok();
+        }
+
+        // Set rental penalty for damaged movie
+        [HttpPut]
+        public async Task<ActionResult> SetRentalPenalty(int rentalId, bool isDamaged, decimal penalty)
+        {
+            await _rentalService.SetPenaltyForDamagedMovieAsync(rentalId, isDamaged, penalty);
+            return Ok();
+        }
+    }
+}
